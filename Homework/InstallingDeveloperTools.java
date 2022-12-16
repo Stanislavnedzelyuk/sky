3b07fded5a6c2327825cc0614d962394788abb8d
Stanislav <stanislav.nedzelyuk@icloud.com>
2022-12-17 00:05:36 +0700

    Установка инструментов разработчика

---
+++ Homework/InstallingDeveloperTools.java
@@ -0,0 +1,8 @@
+package Homework;
+
+public class InstallingDeveloperTools { // Установка инструментов разработчика
+    public static void main(String[] args) {
+        var x =5;
+        System.out.println(x);
+    }
+}
