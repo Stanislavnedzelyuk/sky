501150fafdad36fd44d49662ad62a204e058b5f8
Stanislav <stanislav.nedzelyuk@icloud.com>
2022-12-21 12:03:26 +0700

    Условный оператор 1

---
+++ Homework/String1.java
@@ -0,0 +1,13 @@
+package Homework;
+
+public class String1 {
+    public static void main(java.lang.String[] args) {
+        java.lang.String firstName = "Семён";
+        java.lang.String middleName = "Семёнович";
+        java.lang.String lastName = "Иванов";
+        java.lang.String fullName = lastName + " " + middleName + " " + firstName;
+        System.out.println("ФИО сотрудника -" + " " + fullName);
+        System.out.println("Данные ФИО сотрудника для заполнения отчета —" + " " + fullName.toUpperCase());
+        System.out.println("Данные ФИО сотрудника —" + " " + fullName.replace("ё", "е" ));
+    }
+}
